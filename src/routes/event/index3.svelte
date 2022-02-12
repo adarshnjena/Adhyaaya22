6b27a600c3c88424095118d9d2f6cb2e131b22ce
Dev Parapalli <devparapalli@gmail.com>
2022-02-12 23:32:00 +0530

    Text in the scrollable container

--- src/routes/event/index3.svelte
+++ src/routes/event/index3.svelte
@@ -14,7 +14,7 @@
     import Icon from '@iconify/svelte/dist/OfflineIcon.svelte';
     import { getApp, initializeApp } from 'firebase/app';
     import { onMount } from 'svelte';
-
+    import {frame_data} from'./index3_data'
     let app;
     let innerHeight: number;
     let scrollY: number; // Number of Pixels scrolled
@@ -31,11 +31,7 @@
                 container?.getBoundingClientRect().bottom -
                     container?.getBoundingClientRect().top || 0;
             const pixelsPerImage = containerHeight / numberOfImages;
-            image_index = Math.ceil(scrollY / pixelsPerImage);
-            if (image_index >= numberOfImages) {
-                // Limit the image index to the number of images
-                image_index = numberOfImages;
-            }
+            image_index = Math.min(Math.floor(scrollY / pixelsPerImage), numberOfImages);
             if (image_index <= 0) {
                 // Limit the image index to the number of images
                 image_index = 1;
@@ -84,18 +80,24 @@
         <div class="content">
             <p class="hidden">This site is best experienced on a 16:9 Monitor.</p>
             <img src="{src}" alt="Showcase" class="min-h-screen w-full" />
+            <div class="text-container">
+                <div class="subtitle {frame_data[image_index]?.line_1[1] || 'invisible'}">{frame_data[image_index]?.line_1[0]}</div>
+                <div class="title {frame_data[image_index]?.line_2[1]|| 'invisible'}">{frame_data[image_index]?.line_2[0]}</div>
+                <div class="title {frame_data[image_index]?.line_3[1]|| 'invisible'}">{frame_data[image_index]?.line_3[0]}</div>
+                <div class="subtitle {frame_data[image_index]?.line_4[1]|| 'invisible'}">{frame_data[image_index]?.line_4[0]}</div>
+            </div>
         </div>
     </div>
-    <div class="not-scroll-bound flex snap-start snap-always justify-center bg-base-300">
+    <div class="min-h-screen sticky top-0 flex-col justify-center items-center flex snap-start snap-always bg-base-300">
         <span class="text-white">CONTENT 1</span>
     </div>
-    <div class="not-scroll-bound flex snap-start snap-always justify-center bg-base-100">
+    <div class="min-h-screen sticky top-0 flex-col justify-center items-center flex snap-start snap-always bg-base-100">
         <span class="text-white">CONTENT 2</span>
     </div>
-    <div class="not-scroll-bound flex snap-start snap-always justify-center bg-base-300">
+    <div class="min-h-screen sticky top-0 flex-col justify-center items-center flex snap-start snap-always bg-base-300">
         <span class="text-white">CONTENT 3</span>
     </div>
-    <div class="not-scroll-bound flex snap-start snap-always justify-center bg-base-100">
+    <div class="min-h-screen sticky top-0 flex-col justify-center items-center flex snap-start snap-always bg-base-100">
         <span class="text-white">CONTENT 4</span>
     </div>
 </div>
@@ -143,15 +145,25 @@
         }
     }
 
-    .not-scroll-bound {
-        min-height: 100vh;
+    .text-container {
         width: 100%;
-        position: sticky;
-        top: 0;
+        height: 100%;
+        position: fixed;
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
+        top: 0;
+        color: white;
+        .subtitle {
+            //opacity: 0;
+            font-size: 30px;
+        }
+        .title {
+            //opacity: 0;
+            font-size: 80px;
+            margin: -20px 0;
+        }
     }
     .custom-background {
         background-image: var(--bga), var(--bgb);

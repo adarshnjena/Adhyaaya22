64f3cc622803126824f8a0bc770b72392dc45c22
DevParapalli <parapallidev@gmail.com>
2022-02-28 14:10:31 +0530

    moved feedback into its own folder + netlify

--- src/lib/index/NonImportantFeedBackIframe.svelte
+++ src/lib/index/NonImportantFeedBackIframe.svelte
@@ -1,18 +1,17 @@
 <script>
     import { onMount } from 'svelte';
-    let scrollY = 0;
+    //let scrollY = 0;
     onMount(() => {
         // Disabled Scroll thingy
         //scrollY = 100;
     });
 </script>
 
-<svelte:window bind:scrollY />
 
 <!--This was a retarded attempt at trying to convert this code. Broken Click-->
 
 <iframe
-    src="/_index/nip_feedback.html"
+    src='{`/feedback/nip.html`}'
     class="tw-h-screen tw-w-screen tw-overflow-clip tw-bg-base-200"
     title="Page was written using jQuery, Bootstrap."
     frameborder="0"

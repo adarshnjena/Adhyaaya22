b806514131dc3d18a19390feda6d3b3e0ccbc49f
Siddhant Patil <shimpli.patil2804@gmail.com>
2022-03-01 20:57:55 +0530

    Adding Teams page in iframe

---
+++ src/routes/events/teams.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { onMount } from 'svelte';
+    let scrollY = 0;
+    onMount(() => {
+        //scrollY = 84;
+    });
+</script>
+
+<svelte:window bind:scrollY />
+
+<iframe
+    src="/teams/index.html"
+    class="tw-h-screen tw-w-screen tw-overflow-clip"
+    title="Events Page, Uses external GSAP"
+    frameborder="0"
+></iframe>

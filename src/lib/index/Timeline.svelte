42d1f184f33cb7e6a3c7d4f3fad03dbf0ac8462d
Siddhant Patil <shimpli.patil2804@gmail.com>
2022-03-01 18:09:59 +0530

    Dates updated

--- src/lib/index/Timeline.svelte
+++ src/lib/index/Timeline.svelte
@@ -61,7 +61,7 @@ import { browser } from '$app/env';
         </li>
         <li>
             <div>
-                <time>1900</time>
+                <time>24th March</time>
                 <div class="discovery">
                     <h1>Inauguration Ceremony</h1>
                     <p></p>
@@ -70,7 +70,7 @@ import { browser } from '$app/env';
         </li>
         <li>
             <div>
-                <time>1905</time>
+                <time>24th March</time>
                 <div class="discovery">
                     <h1>Day 1</h1>
                     <p></p>

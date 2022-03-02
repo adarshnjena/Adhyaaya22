e50ec5c1eea91b132945e5032de2b659d04ddc7d
Dev Parapalli <devparapalli@gmail.com>
2022-03-02 09:46:55 +0530

    Fixed missing lang declaration in EventNavbar.svelte

--- src/routes/event/EventNavbar.svelte
+++ src/routes/event/EventNavbar.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     import { onMount } from 'svelte';
     import Icon from '@iconify/svelte/dist/OfflineIcon.svelte';
     import outlineLogout from '@iconify-icons/ic/outline-logout.js';

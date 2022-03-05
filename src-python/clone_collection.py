cc9349b1c7214e91c1a9fd065624c762a9a9f60a
Dev Parapalli <devparapalli@gmail.com>
2022-03-05 13:10:54 +0530

    Added code to export the data for ca_code, plus datetime increments for the exports

--- src-python/clone_collection.py
+++ src-python/clone_collection.py
@@ -3,6 +3,7 @@
 from firebase_admin import firestore
 import json
 from dateutil.parser import parse
+from time import time
 import firebase_admin
 from firebase_admin import credentials
 import pathlib
@@ -15,8 +16,8 @@ parent_folder = this_folder.parent
 # Replace this value with the collection you would like to clone
 COLLLECTION_NAME = 'registrations'
 
-JSON_EXPORT_FILE = parent_folder / f'{COLLLECTION_NAME}.json'
-CSV_EXPORT_FILE = parent_folder / f'{COLLLECTION_NAME}.csv'
+JSON_EXPORT_FILE = parent_folder / f'{COLLLECTION_NAME}-{int(float(time())*100)}.json'
+CSV_EXPORT_FILE = parent_folder / f'{COLLLECTION_NAME}-{int(float(time())*100)}.csv'
 # This is a service account for firebase.
 # You will need to download and save this file next to this script before running the script.
 # To generate a private key file for your service account:
---
+++ src-python/sync_refferal_codes.py
@@ -0,0 +1,75 @@
+from firebase_admin import firestore
+import json
+from dateutil.parser import parse
+import firebase_admin
+from firebase_admin import credentials
+import pathlib
+
+this_folder = pathlib.Path(__file__).parent.absolute()
+parent_folder = this_folder.parent
+
+FIREBASE_SA_KEY = this_folder / 'serviceAccountKey.json'
+FIREBASE_CA_SA_KEY = this_folder / 'serviceAccountKeyCA.json'
+
+print('[INFO] Authenticating MAIN...')
+main_cred = credentials.Certificate(FIREBASE_SA_KEY)
+main_app = firebase_admin.initialize_app(main_cred, name='MAIN')
+
+print('[INFO] Authenticating CA...')
+ca_cred = credentials.Certificate(FIREBASE_SA_KEY)
+ca_app = firebase_admin.initialize_app(ca_cred, name='CA')
+
+print('[INFO] Instantiating Client MAIN')
+main_client = firestore.client(main_app)
+
+print('[INFO] Instantiating Client CA')
+ca_client = firestore.client(ca_app)
+
+print('[INFO] Cloning Registrations...')
+
+
+regs = {}
+print('[INFO] Writing JSON Document...')
+with open(parent_folder / f'main_registrations.json', 'w') as f:
+    print('[INFO] Fetching Registrations...')
+    snapshots = list(main_client.collection('registrations').get())
+    print('[INFO] Registraions Fetched')
+    for snapshot in snapshots:
+        regs[snapshot.id] = snapshot.to_dict()
+        
+    f.write(json.dumps(regs, indent=4))
+    # f.write(json.dumps(k))
+print('[INFO] Finished Writing JSON Document')
+
+trans = {}
+print('[INFO] Writing JSON Document...')
+with open(parent_folder / 'main_transactions.json', 'w') as f:
+    print('[INFO] Fetching Transactions...')
+    snapshots = list(main_client.collection('transactions').get())
+    print('[INFO] Transactions Fetched')
+    for snapshot in snapshots:
+        trans[snapshot.id] = snapshot.to_dict()
+        
+    f.write(json.dumps(trans, indent=4))
+    # f.write(json.dumps(k))
+print('[INFO] Finished Writing JSON Document')
+
+
+# At this point, k contains all the registrations.
+# parse the json file and get all counts of ca_codes
+ca_codes = {}
+for user_id, user_registrations in regs.items():
+        # print(key) # This is user_id,
+        # print(value) # This is a dict of all the registrations
+        for event_code, registration in user_registrations.items():
+            # print('[INFO] CA CODE: ', registration['refferal_code'], registration['registration_id'])
+            if len(registration['refferal_code']) == 0:
+                continue
+            # update the count for that specific refferal code.
+            # if the refferal code is not in the dict, add it.
+            if registration['refferal_code'] not in ca_codes:
+                ca_codes[registration['refferal_code']] = 1
+            else:
+                ca_codes[registration['refferal_code']] += 1
+
+print(json.dumps(ca_codes, indent=4))

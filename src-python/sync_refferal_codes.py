259cc57ad26a037e18d3c00d7c089e0c00b0e9d4
Dev Parapalli <devparapalli@gmail.com>
2022-03-08 07:41:30 +0530

    Added yarn data command and updated main.py to contain the logic

--- src-python/sync_refferal_codes.py
+++ src-python/sync_refferal_codes.py
@@ -14,11 +14,11 @@ FIREBASE_CA_SA_KEY = this_folder / 'serviceAccountKeyCA.json'
 
 print('[INFO] Authenticating MAIN...')
 main_cred = credentials.Certificate(FIREBASE_SA_KEY)
-main_app = firebase_admin.initialize_app(main_cred, name='MAIN')
+main_app = firebase_admin.initialize_app(main_cred, name='MAIN2')
 
 print('[INFO] Authenticating CA...')
 ca_cred = credentials.Certificate(FIREBASE_CA_SA_KEY)
-ca_app = firebase_admin.initialize_app(ca_cred, name='CA')
+ca_app = firebase_admin.initialize_app(ca_cred, name='CA2')
 
 print('[INFO] Instantiating Client MAIN')
 main_client = firestore.client(main_app)
@@ -59,19 +59,21 @@ print('[INFO] Finished Writing JSON Document')
 # At this point, k contains all the registrations.
 # parse the json file and get all counts of ca_codes
 ca_codes = {}
-for user_id, user_registrations in regs.items():
-        # print(key) # This is user_id,
-        # print(value) # This is a dict of all the registrations
-        for event_code, registration in user_registrations.items():
-            # print('[INFO] CA CODE: ', registration['refferal_code'], registration['registration_id'])
-            if len(registration['refferal_code']) == 0:
-                continue
-            # update the count for that specific refferal code.
-            # if the refferal code is not in the dict, add it.
-            if registration['refferal_code'] not in ca_codes:
-                ca_codes[registration['refferal_code']] = 1
-            else:
-                ca_codes[registration['refferal_code']] += 1
+with open(parent_folder / f'ca_codes-{int(float(time())*100)}.json', 'w') as f:
+    for user_id, user_registrations in regs.items():
+            # print(key) # This is user_id,
+            # print(value) # This is a dict of all the registrations
+            for event_code, registration in user_registrations.items():
+                # print('[INFO] CA CODE: ', registration['refferal_code'], registration['registration_id'])
+                if len(registration['refferal_code']) == 0:
+                    continue
+                # update the count for that specific refferal code.
+                # if the refferal code is not in the dict, add it.
+                if registration['refferal_code'] not in ca_codes:
+                    ca_codes[registration['refferal_code']] = 1
+                else:
+                    ca_codes[registration['refferal_code']] += 1
+    f.write(json.dumps(ca_codes, indent=4))
 
 print('[INFO] WRITING TO DB')
 batch = ca_client.batch()
@@ -79,7 +81,7 @@ batch = ca_client.batch()
 
 for code, count in ca_codes.items():
     iter_ref = ca_client.collection('ca_code').document(code)
-    batch.update(iter_ref, {'number_of_regis': count})
+    batch.set(iter_ref, {'number_of_regis': count})
     # print(code, count)
 
 

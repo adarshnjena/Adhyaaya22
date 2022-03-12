02cbdf57e748a914baf91acb85e73f964c72978b
Dev Parapalli <devparapalli@gmail.com>
2022-03-12 12:02:52 +0530

    updated script to do the thing properly

--- src-python/remove_unverifed.py
+++ src-python/remove_unverifed.py
@@ -14,4 +14,28 @@ FIREBASE_CA_SA_KEY = this_folder / 'serviceAccountKeyCA.json'
 
 print('[INFO] Authenticating MAIN...')
 main_cred = credentials.Certificate(FIREBASE_SA_KEY)
-main_app = firebase_admin.initialize_app(main_cred, name='MAIN2')
+main_app = firebase_admin.initialize_app(main_cred, name='MAIN3')
+
+print('[INFO] Instantiating Client')
+client = firestore.client(app=main_app)
+
+
+COLLECTION_NAME = 'registrations'
+JSON_EXPORT_FILE = parent_folder / f'unverified_registrations-{int(float(time())*100)}.json'
+
+with open(JSON_EXPORT_FILE, 'w', encoding="utf-8") as f:
+    print('[INFO] Fetching Registrations...')
+    snapshots = list(client.collection(COLLECTION_NAME).get())
+    print('[INFO] Registraions Fetched')
+    for snapshot in snapshots:
+        for event_code, registration in snapshot.to_dict().items():
+            if registration['transaction_status'] != 'PAID':
+                f.write(json.dumps(registration, indent=4))
+                f.write('\n')
+                current_ref = client.collection(COLLECTION_NAME).document(snapshot.id)
+                current_ref.update({
+                    f'{event_code}': firestore.DELETE_FIELD
+                })
+                print(registration)
+    # f.write(json.dumps(k))
+print('[INFO] Finished Writing JSON Document')
